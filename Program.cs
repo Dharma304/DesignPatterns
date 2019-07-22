87b85de11b3e79d6cc462b31aa00eda39ea58b01
Dharmendra Kumar <dharma304@outlook.com>
2019-07-23 03:47:39 +0530

    Strategy class added

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            strategy obj=new strategy();
+            obj.Show();
         }
     }
 }
---
+++ strategy.cs
@@ -0,0 +1,11 @@
+using System;
+namespace DesignPattern
+{
+    public class strategy
+    {
+        public  void Show()
+        {
+            Console.WriteLine("Hello Startegy Pattern");
+        }
+    }
+}
